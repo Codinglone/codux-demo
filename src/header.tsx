9ee91b5bf18960197f61d4454a9190e06f8fffbe
Fabrice <codinglone@gmail.com>
2023-01-18 07:12:15 +0200

    Initial commit

--- src/header.tsx
+++ src/header.tsx
@@ -1,6 +1,5 @@
 import type React from 'react';
 import { st, classes } from './header.st.css';
-import stylableLogo from './assets/stylable.svg';
 
 export interface HeaderProps {
     className?: string;
@@ -9,18 +8,13 @@ export interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({ className }) => {
     return (
         <header className={st(classes.root, className)}>
-            <h1 className={classes.title}>
-                Hello{' '}
+            <h1 className={classes.title}>Hi{' '}
                 <a
                     className={classes.anchor}
                     href="https://stylable.io"
                     target="_blank"
                     rel="noopener noreferrer"
-                >
-                    Stylable!
-                </a>{' '}
-                <img className={classes.icon} src={stylableLogo} width={50} height={50} alt="" />
-            </h1>
+                >There</a>{' '}</h1>
         </header>
     );
 };
